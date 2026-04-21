8436e271bda24f54d819a593b602e4df3f249fb5
Konstantin Ullrich <konstantinullrich12@gmail.com>
2026-04-21 12:00:30 +0200

    Initial Commit

---
+++ lib/src/utils/exception_utils.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/services.dart';
+import 'package:trezor_flutter/src/exceptions/trezor_exception.dart';
+
+import '../models/connection_type.dart';
+
+class TrezorExceptionUtils {
+  const TrezorExceptionUtils._();
+
+  static TrezorException fromPlatformException(
+    PlatformException exception,
+    ConnectionType connectionType,
+  ) {
+    final errorCode = int.tryParse(exception.code) ?? 0;
+    final message = exception.message ?? '';
+
+    if (message == "connectionLost") {
+      return ConnectionLostException(connectionType: connectionType);
+    }
+    return LedgerDeviceException(
+      errorCode: errorCode,
+      message: message,
+      cause: exception,
+      connectionType: connectionType,
+    );
+  }
+}
